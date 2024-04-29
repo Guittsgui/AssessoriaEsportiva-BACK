ea62b6a0a362996bc755610f7be51ccd07c1a83b
Guilherme Tavares e Tavares Silva <guittsilva@MacBook-Pro-de-Guilherme.local>
2024-04-29 11:27:00 -0300

    FIX: Refatorar nodemailer para transformar em classe.

--- src/libs/nodemailer.ts
+++ src/libs/nodemailer.ts
@@ -1,23 +1,39 @@
 import nodemailer from 'nodemailer'
 
 
-const transport = nodemailer.createTransport({
-    host: "sandbox.smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-      user: "400c891be64015",
-      pass: "d1a5055b76b002"
-    }
-  });
+class NodeMailer{
 
-  const message = {
-    from: `joao <email>`,
-    to: 'testeapi@hotmail.com',
-    subject: 'Email via Site',
-    text: ""
+  transport: nodemailer.Transporter
+
+  constructor(){
+    this.transport = nodemailer.createTransport({
+      host: "sandbox.smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+        user: "400c891be64015",
+        pass: "d1a5055b76b002"
+      }
+    });
+  }
+
+
+  async executeEmailSending(){
+    const message = this.buildMessage();
+    const info = this.transport.sendMail
+  }
+
+  private buildMessage(){
+    const newMessage = ""
+    //     from: `joao <email>`,
+    //     to: 'testeapi@hotmail.com',
+    //     subject: 'Email via Site',
+    //     text: ""
+
+    return newMessage
+  }
+
 }
 
-export async function executeEmailSending(){
-    const info = transport.sendMail(message)
-    
-}
+export default NodeMailer
+
+
