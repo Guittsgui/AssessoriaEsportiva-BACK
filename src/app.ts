11105ad60dab46b6011a76dca1b588096071ce1a
Guilherme Tavares e Tavares Silva <guittsilva@MacBook-Pro-de-Guilherme.local>
2024-04-30 10:49:26 -0300

    FIX: Refatorar SERVER, para que o subir a aplicacao fique longe da configuracao do server, Implementar Classe

---
+++ src/app.ts
@@ -0,0 +1,48 @@
+import express from 'express'
+import path from 'path';
+import cors from 'cors'
+import newsLetterRouter from './routes/newsLetterRoutes';
+import userRouter from './routes/userRoutes';
+import bodyParser from 'body-parser';
+import contacEmailRoutes from './routes/contacEmailRoutes';
+import morgan from 'morgan'
+
+import dotenv from 'dotenv'
+
+dotenv.config();
+class App{
+
+    private app: express.Application
+    constructor(){
+        this.app = express();
+        this.configApp();
+        this.routes();    
+    }
+
+    listen(port: number){
+        this.app.listen(process.env.PORT || port, () => {
+            console.log('Server is Running.')
+        })
+    }
+
+    configApp(){
+        this.app.use(cors());
+        this.app.use(bodyParser.json())
+        this.app.use(express.static(path.join(__dirname, '../public')));
+        this.app.use(express.urlencoded({extended: true}));
+        this.app.use(morgan('tiny'));
+    }
+
+    routes(){
+        this.app.use(newsLetterRouter, 
+            userRouter,
+            contacEmailRoutes,
+        )
+        this.app.use((request, response) => {
+            response.status(404);
+            response.json({msg: 'Página Não Encontrada'})
+        })
+    }
+}
+
+export {App}
--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,49 @@
-import express from 'express'
-import path from 'path';
-import dotenv from 'dotenv';
-import cors from 'cors'
-import newsLetterRouter from './routes/newsLetterRoutes';
-import userRouter from './routes/userRoutes';
-import bodyParser from 'body-parser';
-import contacEmailRoutes from './routes/contacEmailRoutes';
-import morgan from 'morgan'
-
+import dotenv from 'dotenv'
 
 dotenv.config();
 
-const server = express();
+import {App} from './app'
 
-server.use(bodyParser.json())
-server.use(cors());
-server.use(express.static(path.join(__dirname, '../public')));
-server.use(express.urlencoded({extended: true}));
-server.use(morgan('tiny'));
-server.use(
-    newsLetterRouter, 
-    userRouter,
-    contacEmailRoutes,
-)
+const app = new App();
 
-server.use((request, response) => {
-    response.status(404);
-    response.json({msg: 'Página Não Encontrada'})
-})
+app.listen(4005)
 
-server.listen(process.env.PORT, () => {
-    console.log('RODOU -- ')
-});
+
+
+
+
+
+// import express from 'express'
+// import path from 'path';
+// import dotenv from 'dotenv';
+// import cors from 'cors'
+// import newsLetterRouter from './routes/newsLetterRoutes';
+// import userRouter from './routes/userRoutes';
+// import bodyParser from 'body-parser';
+// import contacEmailRoutes from './routes/contacEmailRoutes';
+// import morgan from 'morgan'
+
+
+// // dotenv.config();
+
+// // const server = express();
+
+// // server.use(bodyParser.json())
+// // server.use(cors());
+// // server.use(express.static(path.join(__dirname, '../public')));
+// // server.use(express.urlencoded({extended: true}));
+// // server.use(morgan('tiny'));
+// // server.use(
+// //     newsLetterRouter, 
+// //     userRouter,
+// //     contacEmailRoutes,
+// // )
+
+// // server.use((request, response) => {
+// //     response.status(404);
+// //     response.json({msg: 'Página Não Encontrada'})
+// // })
+
+// // server.listen(process.env.PORT, () => {
+// //     console.log('RODOU -- ')
+// // });
